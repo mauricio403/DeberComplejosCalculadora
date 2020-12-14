baf5de5767f607339a10fee694970d29e363bf9b
mauricio403 <mauriciomatango_@hotmail.com>
2020-12-13 21:46:33 -0500

    Multiplicacion por un escalar

--- DeberComplejos/CalculadoraLogica/CalculadoraLogica.cs
+++ DeberComplejos/CalculadoraLogica/CalculadoraLogica.cs
@@ -15,11 +15,78 @@ namespace CalculadoraLogica
 
         public Complejo Restar(Complejo primerNumero, Complejo segundoNumero)
         {
-            var parteReal = primerNumero.Real + segundoNumero.Real;
-            var parteImaginaria = primerNumero.Imaginaria + segundoNumero.Imaginaria;
+            var parteReal = primerNumero.Real - segundoNumero.Real;
+            var parteImaginaria = primerNumero.Imaginaria - segundoNumero.Imaginaria;
             var nuevoComplejo = new Complejo(parteReal, parteImaginaria);
 
             return nuevoComplejo;
         }
+
+        public Complejo Multiplicar(Complejo primerNumero, Complejo segundoNumero)
+        {
+            var primerFactor = primerNumero.Real * segundoNumero.Real;
+            var segundofactor = primerNumero.Real * segundoNumero.Imaginaria;
+            var tercerFactor = primerNumero.Imaginaria * segundoNumero.Real;
+            var cuartoFactor = primerNumero.Imaginaria * segundoNumero.Imaginaria;
+
+            var cambioNegativo = cuartoFactor * -1;
+
+            var parteReal = primerFactor + cambioNegativo;
+            var parteImaginaria = segundofactor + tercerFactor;
+
+            var nuevoComplejo = new Complejo(parteReal, parteImaginaria);
+
+            return nuevoComplejo;
+        }
+
+        /*public Complejo Dividir(Complejo primerNumero, Complejo segundoNumero)
+        {
+            //Creacion de la expresion conjugada
+            var imaginariaConjugada = segundoNumero.Imaginaria * -1;
+
+            var expresionConjugada = new Complejo(segundoNumero.Real, imaginariaConjugada);
+            
+            //Parte del dividendo
+            var primerFactor = primerNumero.Real * segundoNumero.Real;
+            var segundoFactor = primerNumero.Real * expresionConjugada.Imaginaria;
+            var tercerFactor = primerNumero.Imaginaria * segundoNumero.Real;
+            var cuartoFactor = primerNumero.Imaginaria * expresionConjugada.Imaginaria;
+            var cuartoCambio = cuartoFactor * -1;
+
+            var primerDividendo = (primerFactor + cuartoCambio);
+            var segundoDividendo = (segundoFactor + tercerFactor);
+
+            //Parte del divisor
+            var quintoFactor = segundoNumero.Real * segundoNumero.Real;
+            var sextofactor = segundoNumero.Imaginaria * expresionConjugada.Imaginaria;
+
+            var sextoCambio = sextofactor * -1;
+
+            var divisor = quintoFactor + sextoCambio;
+
+
+
+            var parteReal = primerDividendo / divisor;
+
+            var parteImaginaria = segundoDividendo / divisor;
+
+            var nuevocomplejo = new Complejo(parteReal, parteImaginaria);
+
+            return nuevocomplejo;
+
+
+        }*/
+
+        public Complejo multiplicacionEscalar(double numero, Complejo primerNumero)
+        {
+            var parteReal = numero * primerNumero.Real;
+
+            var parteImaginaria = numero * primerNumero.Imaginaria;
+
+            var nuevoComplejo = new Complejo(parteReal, parteImaginaria);
+
+            return nuevoComplejo;
+        }
+
     }
 }
--- DeberComplejos/calculadoraComplejoTest/ComplejosTest.cs
+++ DeberComplejos/calculadoraComplejoTest/ComplejosTest.cs
@@ -42,6 +42,56 @@ namespace calculadoraComplejoTest
 
         }
 
-        
+        [TestMethod]
+
+        public void MultiplicacionComplejos()
+        {
+            var primerComplejo = new Complejo(5, 2);
+            var segundoComplejo = new Complejo(4, 4);
+            var resultadoEsperado = new Complejo(12, 28);
+
+            var micalculadora = new Calculadora();
+
+            var resultadoObtenido = micalculadora.Multiplicar(primerComplejo, segundoComplejo);
+
+            Assert.AreEqual(resultadoEsperado.Real, resultadoObtenido.Real);
+
+            Assert.AreEqual(resultadoEsperado.Imaginaria, resultadoObtenido.Imaginaria);
+        }
+
+        [TestMethod]
+
+        /*public void DivisionComplejos()
+        {
+            var primerComplejo = new Complejo(6, 5);
+            var segundoComplejo = new Complejo(5, 4);
+            var resultadoEsperado = new Complejo(50/29, 1/29);
+
+            var miCalculadora = new Calculadora();
+
+            var resultadoObtenido = miCalculadora.Dividir(primerComplejo, segundoComplejo);
+            
+            Assert.AreEqual(resultadoEsperado.Real, resultadoObtenido.Real);
+
+            Assert.AreEqual(resultadoEsperado.Imaginaria, resultadoObtenido.Imaginaria);
+        }*/
+
+        public void multiplicacionPorescalar()
+        {
+            var escalar = 2;
+
+            var complejo = new Complejo(4, 6);
+
+            var resultadoEsperado = new Complejo(8, 12);
+
+            var micalculadora = new Calculadora();
+
+            var resultadoObtenido = micalculadora.multiplicacionEscalar(escalar, complejo);
+
+            Assert.AreEqual(resultadoEsperado.Real, resultadoObtenido.Real);
+
+            Assert.AreEqual(resultadoEsperado.Imaginaria, resultadoObtenido.Imaginaria);
+
+        }
     }
 }
